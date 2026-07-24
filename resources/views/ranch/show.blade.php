cf82dc516ddc58da9fa2a3f7ecfeb140330769f1
Ibraim Barrios <barrios.party@hotmail.com>
2026-07-24 14:33:33 -0600

    Show property of ranch

--- resources/views/ranch/show.blade.php
+++ resources/views/ranch/show.blade.php
@@ -73,6 +73,16 @@
 
                     <div class="grid grid-cols-1 md:grid-cols-3 py-4">
                         <dt class="font-semibold text-gray-600 dark:text-gray-300">
+                            Propietario o Administrador del Rancho
+                        </dt>
+
+                        <dd class="md:col-span-2 text-gray-900 dark:text-white">
+                            {{ $ranch->user->name }}
+                        </dd>
+                    </div>
+
+                    <div class="grid grid-cols-1 md:grid-cols-3 py-4">
+                        <dt class="font-semibold text-gray-600 dark:text-gray-300">
                             Status
                         </dt>
 
